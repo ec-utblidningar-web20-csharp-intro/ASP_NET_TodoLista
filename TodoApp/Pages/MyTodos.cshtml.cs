6dc42639c87553597d0f76e9b646edc3da46a887
Björn Strömberg <bjorn.stromberg@codic.se>
2021-03-16 11:54:55 +0100

    Vi har en form för att skicka data från client till server. Lägga in nya todos

--- TodoApp/Pages/MyTodos.cshtml.cs
+++ TodoApp/Pages/MyTodos.cshtml.cs
@@ -4,14 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
 
 namespace TodoApp.Pages
 {
     public class MyTodosModel : PageModel
     {
+
+        private readonly ILogger<ErrorModel> _logger;
         public List<Models.Todo> Todos;
-        public MyTodosModel()
+        public MyTodosModel(ILogger<ErrorModel> logger)
         {
+            _logger = logger;
+
             Todos = new List<Models.Todo>()
             {
                 new Models.Todo() { Action = "Köpa mjölk" },
@@ -20,9 +26,20 @@ namespace TodoApp.Pages
                 new Models.Todo() { Action = "Skriva inlämningsrapporten" },
             };
         }
-        // Ha en referens till en ToDo lista
         public void OnGet()
         {
+            _logger.LogInformation("Nu fick vi en GET request");
+        }
+        public void OnPost()
+        {
+            _logger.LogInformation("Nu fick vi en POST request");
+
+            StringValues action = Request.Form["Action"];
+            StringValues isDone = Request.Form["IsDone"];
+            Todos.Add(new Models.Todo() { 
+                Action = action, 
+                IsDone = isDone.ToString() == "on" 
+            });
         }
     }
 }
