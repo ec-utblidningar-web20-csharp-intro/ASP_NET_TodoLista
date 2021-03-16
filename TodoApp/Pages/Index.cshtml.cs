9e1d57f7e0c2d82ac415a1e5284f73fe93c726a1
Björn Strömberg <bjorn.stromberg@codic.se>
2021-03-16 09:50:03 +0100

    Add project files.

---
+++ TodoApp/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            this._logger = logger;
+        }
+
+        // detta är en PageHandler för http GET requests
+        public void OnGet()
+        {
+        }
+
+        // detta är en PageHandler för http OPTION requests
+        public void OnOption()
+        {
+
+        }
+    }
+}
